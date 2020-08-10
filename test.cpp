cb6fd8f1e832ea4906289cda15851ed2bc67bae1
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2020-08-10 23:07:47 +0600

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -24,7 +24,8 @@ struct term												// Experimental area
 	char braceType;										// into algebraic terms.
 														// terms contain a co-efficient 
     vector < pair <char, int> > variable_list; 			// a list of variables along with respective exponent
-    int constant;										// and a short chek if its a constant term or not
+	string leadingOperator;								// and a short chek if its a constant term or not
+    int constant;										// and a string to contain the leading opearator
 };														// for the sake of handling braces; braces will also be included
 														// in the term structure
 vector <term> term_container;							// GLOBALLY MANAGED CONTAINER FOR TERMS
@@ -99,7 +100,9 @@ int token_to_int(string temp){
 }
 
 ////// EXPERIMENTAL AREA //////
-term make_term(vector <int> type, vector <string> token, int size){
+
+// make term takes a small list of toekns and returns a term structure//
+term make_term(vector <int> type, vector <string> token, int size, string leadingOperator){
 	struct term finalTerm;              //...........................................// final return value
 
 	bool foundExponetSign = false;		//...........................................// boolean trigger for exponential sign token
@@ -122,6 +125,7 @@ term make_term(vector <int> type, vector <string> token, int size){
 		}
 		// end of special if step	
 
+
 		// initially storing in temporary container 
 		if (type[i] == NUMBER)
 		{ 
@@ -138,10 +142,17 @@ term make_term(vector <int> type, vector <string> token, int size){
 		}
 		else if (type[i] == VARIABLE)
 		{
-			variableChar = token[i][0];
-			foundVariable = true;
-
-			if ( i < size - 1 ) continue;
+			if (!foundVariable)
+			{
+				variableChar = token[i][0];
+				foundVariable = true;
+					
+				if (i < size - 1)	continue;
+			}
+			else if (foundVariable)
+			{
+				i--;	//........................................................// repeate this iteration
+			}
 		}
 		// end of the step
 
@@ -163,15 +174,111 @@ term make_term(vector <int> type, vector <string> token, int size){
 					foundNumber = false;
 				}
 			}
-		}
+			else
+			{
+				finalTerm.variable_list.push_back(make_pair(variableChar, 1));
 
+				variableChar = '\0';
+				value = 0;
+
+				foundVariable = false;
+				foundNumber = false;
+				foundExponetSign = false;
+			}
+			
+		}
+		else if (foundNumber)
+		{
+			if (finalTerm.constant == 0) finalTerm.constant = value;
+			else 						 finalTerm.constant *= value;
+		
+			foundNumber = false;
+		}
 	}
 
 	return finalTerm;
 }
+// end of make term //
+
+// start of packing tokens //
+
+void chek_packing(vector<int> types, vector<string> tokens, string leadingOP){
+	cout << " A Pack with leading op: " << leadingOP << endl; 
+	for (int i=0; i<types.size(); ++i){
+		cout << types[i] << "\t" << tokens[i] << endl;
+	}
+}
+
+
+void getAndPackTokens(string inpt){
+    vector<string> tokens;
+    vector<int> types;
+    
+    strcpy(input, inpt.c_str());
+    prog = input;
+
+    while(1){
+		if((prog - input) >= strlen(input)) break;
+		get_token();
+
+        string demo;
+        demo.assign(token);
+
+        if(token_type >= 1 && token_type <= 3){
+		    tokens.push_back(demo);
+            types.push_back(token_type);
+        }
+	}
+
+	vector <int> pack_types;
+	vector<string> pack_tokens;
+	string leadingOperator = "+";
+
+    for(int i=0; i<tokens.size(); ++i)
+    {
+        if(types[i] == DELIMITER || i == types.size() - 1)
+		{
+			if(pack_types.size() > 0) //..........................................................................................// chaking if pack already has something in before prcessing for the new delimiter
+			{	
+				chek_packing(pack_types, pack_tokens, leadingOperator);
+				//term_container.push_back(make_term(pack_types, pack_tokens, pack_types.size(), leadingOperator));
+			
+				pack_tokens.clear();
+				pack_types.clear();
+			}
+
+			if(tokens[i][0] == '=') continue; //................................................................................// go to the next container if it has a equal sign
+
+			if(strchr("(){}[]", tokens[i][0])) //...............................................................................// special term containing only braces
+			{
+				pack_types.push_back(types[i]);
+				pack_tokens.push_back(tokens[i]);
+
+				chek_packing(pack_types, pack_tokens, "none");
+				//term_container.push_back(make_term(pack_types, pack_tokens, 1));
+				
+				pack_tokens.clear();
+				pack_types.clear();
+
+				continue;
+			} //.................................................................................................................// End of special if
+			else
+			{
+				leadingOperator = tokens[i];
+				continue;
+			}
+		}
+
+		pack_types.push_back(types[i]);    //....................................................................................// standard packing of token types
+		pack_tokens.push_back(tokens[i]);  //....................................................................................// standard packing of tokens
+    }
+
+}
 
 
 
+
+// End of packing tokens //
 
 
 
@@ -221,7 +328,7 @@ void test(){
 	string inpt;
     getline(cin, inpt, '\n');
 
-	getToken_chk(inpt);
+	getAndPackTokens(inpt);
 }
 
 ///// TESTING AREA /////
