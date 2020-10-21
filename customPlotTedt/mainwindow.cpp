f57dfd61eda1b5f5f4ceb513408e860bd84a3e1a
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2020-10-21 10:41:47 +0600

    Add files via upload

---
+++ customPlotTedt/mainwindow.cpp
@@ -0,0 +1,50 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->plot->addGraph();
+    ui->plot->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    //ui->plot->graph(0)->setLineStyle(QCPGraph::lsNone);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::addPoint(double x, double y)
+{
+    qv_x.append(x);
+    qv_y.append(y);
+}
+
+void MainWindow::clearData()
+{
+    qv_x.clear();
+    qv_y.clear();
+}
+
+void MainWindow::plot()
+{
+    ui->plot->graph(0)->setData(qv_x, qv_y);
+    ui->plot->replot();
+    ui->plot->update();
+}
+
+
+void MainWindow::on_btn_add_clicked()
+{
+    addPoint(ui->bx_x->value(), ui->bx_y->value());
+    plot();
+}
+
+void MainWindow::on_btn_clear_clicked()
+{
+    clearData();
+    plot();
+}
---
+++ customPlotTedt/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void addPoint(double x, double y);
+    void clearData();
+    void plot();
+
+private slots:
+    void on_btn_add_clicked();
+
+    void on_btn_clear_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QVector<double> qv_x, qv_y;
+
+};
+#endif // MAINWINDOW_H
