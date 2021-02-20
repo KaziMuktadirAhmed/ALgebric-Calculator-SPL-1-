76a46fcc689f6ecdf8a852140db1df7de010d490
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2021-02-20 13:42:14 +0600

    get_term() .......
    hope this does the trick

--- test_parse_term.cpp
+++ test_parse_term.cpp
@@ -173,16 +173,16 @@ public:
     Term(/* args */) {}
     ~Term() {}
 
-	bool isEqualSign;
-    bool isOperator;
+	bool isEqualSign = false;
+    bool isOperator = false;
     string awperator;
 
-    bool isBrace;
+    bool isBrace = false;
     string brace;
 
-    bool isConstant;
+    bool isConstant = false;
 
-    int co_efficient;
+    int co_efficient = 1;
     vector< pair <string, int> > variable_and_exponent;
 };
 
@@ -197,10 +197,21 @@ private:
 	const int EQUAL_SIGN = 6;
 
 	int start_index = 0;
+	bool get_term_flag = false;
 
 	Tokenizer tokenized_input;
 
-	Term get_term()
+	int token_to_int(string temp)
+	{
+    	int toNum = 0;
+
+    	for	(int i=0; temp[i] != '\0'; ++i)
+        	toNum = toNum*10 + (temp[i]-'0');
+    
+    	return toNum;
+	}
+
+	Term get_term(int token_count)
 	{
 		Term demo;
 
@@ -232,11 +243,117 @@ private:
 			++start_index;
 		}
 		else {
-			
-		} 
+			if (tokenized_input.types[start_index] == NUMBER) {
+				if (start_index == token_count - 1) {
+					demo.isOperator = false;
+					demo.isEqualSign = false;
+					demo.isBrace = false;
+					demo.isConstant = true;
 
+					demo.co_efficient = token_to_int(tokenized_input.tokens[start_index]);
+					++start_index;
+				}
+				else if (tokenized_input.types[start_index + 1] != VARIABLE) {
+					demo.isOperator = false;
+					demo.isEqualSign = false;
+					demo.isBrace = false;
+					demo.isConstant = true;
+
+					demo.co_efficient = token_to_int(tokenized_input.tokens[start_index]);
+
+					if (tokenized_input.types[start_index + 1] == EXPONENT_SIGN)
+						get_term_flag = true;
+					
+					++start_index;
+				}
+				else {
+					demo.isOperator = false;
+					demo.isEqualSign = false;
+					demo.isBrace = false;
+					demo.isConstant = false;
+
+					demo.co_efficient = token_to_int(tokenized_input.tokens[start_index]);   /* Separating the constant in term */
+					++start_index;
+
+					while (start_index < token_count) {
+						if (tokenized_input.types[start_index] == OPERATOR || tokenized_input.types[start_index] == BRACES || tokenized_input.types[start_index] == EQUAL_SIGN)
+							break;
+
+						if (tokenized_input.types[start_index] == VARIABLE) {
+							pair<string, int> temp;
+							temp.first = tokenized_input.tokens[start_index];
+							//++start_index;
+
+								if (tokenized_input.types[start_index + 1] == EXPONENT_SIGN && start_index + 1 < token_count) {
+									++start_index;
+
+									if (tokenized_input.types[start_index + 1] == NUMBER && start_index + 1 < token_count) {
+										++start_index;
+										temp.second = token_to_int(tokenized_input.tokens[start_index]);
+									}
+								}
+								else {
+									temp.second = 1;
+								}
+
+							demo.variable_and_exponent.push_back(temp);
+							++start_index;
+						}
+					}
+				}
+			}
+			else if (tokenized_input.types[start_index] == EXPONENT_SIGN) {
+				if (get_term_flag) {
+					demo.isOperator = true;
+					demo.isEqualSign = false;                                     /* Separating  the special exponent operator */
+					demo.isBrace = false;
+					demo.isConstant = false;
+
+					demo.awperator = tokenized_input.tokens[start_index];
+					++start_index;
+				}
+			}
+			else if (tokenized_input.types[start_index] == VARIABLE) {
+				demo.isOperator = false;
+				demo.isEqualSign = false;
+				demo.isBrace = false;
+				demo.isConstant = false;
+
+				while (start_index < token_count) {
+					if (tokenized_input.types[start_index] == OPERATOR || tokenized_input.types[start_index] == BRACES || tokenized_input.types[start_index] == EQUAL_SIGN)
+						break;
+
+					if (tokenized_input.types[start_index] == VARIABLE) {
+						pair<string, int> temp;
+						temp.first = tokenized_input.tokens[start_index];
+						//++start_index;
+
+						if (tokenized_input.types[start_index + 1] == EXPONENT_SIGN && start_index + 1 < token_count) {
+							++start_index;
+
+							if (tokenized_input.types[start_index + 1] == NUMBER && start_index + 1 < token_count) {
+								++start_index;
+								temp.second = token_to_int(tokenized_input.tokens[start_index]);
+							}
+						}
+						else {
+							temp.second = 1;
+						}
+
+						demo.variable_and_exponent.push_back(temp);
+						++start_index;
+					}
+				}
+
+			}
+		} 
 		
 		return demo;
+	}
+
+	void clear_pair(pair <char, int> &pr) {
+		pr.first = '\0';
+		pr.second = 0;
 	}
 
 public:
@@ -247,9 +364,15 @@ public:
 
 	void take_input(string user_input)
 	{
+		Term temp;
+
 		tokenized_input.start(user_input);
 		tokenized_input.testTokenizer();
-		get_term();
+		temp = get_term(tokenized_input.tokens.size());
+
+		cout << temp.co_efficient << " ";
+		for(int i=0; i<temp.variable_and_exponent.size(); ++i)
+			cout << temp.variable_and_exponent[i].first << "^" << temp.variable_and_exponent[i].second << " "; 
 	}	
 
 };
